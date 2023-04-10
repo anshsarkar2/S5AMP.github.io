181dc9d664e3cabe1a79d27296a937c336e84bbd
Ansh Sarkar <120781733+anshsarkar2@users.noreply.github.com>
2023-04-10 18:02:53 +0530

    Login

---
+++ login/index.js
@@ -0,0 +1,35 @@
+function checkCode() {
+    // Get the entered code
+    var code = document.getElementById("code").value;
+
+    // Load the list of valid codes from the JSON file
+    fetch('valid_codes.json')
+        .then(response => response.json())
+        .then(data => {
+            // Check if the entered code is valid
+            if (data.codes.includes(code)) {
+                // Save the code and the current date and time to the JSON file
+                var loginData = {
+                    code: code,
+                    timestamp: new Date().toISOString()
+                };
+                fetch('login_data.json', {
+                    method: 'POST',
+                    headers: {
+                        'Content-Type': 'application/json'
+                    },
+                    body: JSON.stringify(loginData)
+                })
+                .then(() => {
+                    // Redirect to the success page
+                    window.location.href = 'https://anshsarkar2.github.io/S5AMP.github.io/index.html';
+                })
+                .catch(error => {
+                    console.error('Error saving login data:', error);
+                });
+            } else {
+                // Show an error message
+                alert('Invalid code!');
+            }
+        });
+}
